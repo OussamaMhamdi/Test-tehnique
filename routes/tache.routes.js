770b1a95e9bafabaf396ceb3c3c8a7110a451dde
OussamaMhamdi <36000808+OussamaMhamdi@users.noreply.github.com>
2021-06-02 17:58:13 +0200

    update

--- routes/tache.routes.js
+++ routes/tache.routes.js
@@ -5,15 +5,15 @@ const { authenticateToken } = require('../Core/jwt');
 const tache = require('../models/tache');
 
 
-router.get('/', authenticateToken, tacheController.Get_Taches);
+router.get('/', tacheController.Get_Taches);
 
-router.post('/create', authenticateToken, tacheController.Add_Tache);
+router.post('/create', tacheController.Add_Tache);
 
-router.get('/', authenticateToken, tacheController.Get_comments);
+router.get('/comments', tacheController.Get_comments);
 
-router.post('/createComment', authenticateToken, authenticateToken, tacheController.Add_comment);
+router.post('/createComment',  tacheController.Add_comment);
 
-router.post('/addComment', authenticateToken, tacheController.Add_Comment_to_Tache);
+router.post('/addComment',  tacheController.Add_Comment_to_Tache);
 
 router.patch('/updateStatus/:id', authenticateToken, tacheController.Get_Tache, tacheController.ChangeStutas);
 
